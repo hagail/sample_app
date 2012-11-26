ae82a3fc3cf6a51478b449634f25f8b741cf0ef1
Hagai Levin <hagai.levin@klarna.com>
2012-11-26 18:43:33 +0200

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -54,6 +54,6 @@ describe "Static pages" do
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
-    page.should have_selector 'title', text: full_title('Sign up')
+    page.should have_selector 'title', text: full_title('')
   end
 end
